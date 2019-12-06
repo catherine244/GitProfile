dbc33b15c77ea53ebbf48b13790b0a4625c106cd
catherine244 <ccayreen24@gmail.com>
2019-12-06 14:38:26 +0300

    added a repo class

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,12 +1,9 @@
 export class Repos {
-    repoName:string;
-    description:string;
-    language:string;
-    html:any;
+   
     
-    // public name:string;
-    // public html_url:string;
-    // public description:string;
-    // public created_at:Date;
+    public name:string;
+    public html_url:string;
+    public description:string;
+    public created_at:Date;
 }
 
