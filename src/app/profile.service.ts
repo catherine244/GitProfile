b92d0d04688731fb4a40827405421bf5040a6ff6
catherine244 <ccayreen24@gmail.com>
2019-12-06 09:14:24 +0300

    added service.ts

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -8,6 +8,18 @@ export class ProfileService {
   private clientid = "8c72951d2de9687a48aa"
   private clientsecret = "edfb2322c4ad62920e40429e8bb7d1cdd50ddcbe";
   
-    
+  constructor(private http:HttpClient) { 
+    this.username = "catherine244";
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id="+ this.clientid + "&client_secret=" + this.clientsecret)
+  }
+
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.clientid + "&client_secret=" + this.clientsecret)
+  }
+  updateProfile(username:string){
+    this.username = username;
+  }
   }
 
